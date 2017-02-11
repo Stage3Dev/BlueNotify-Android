1c83e375673bb7099c80c115450a6a94bf7a2c2f
Kyle Thompson <thewkbrdguy@gmail.com>
2017-02-11 16:35:27 -0600

    Add notification listener service

---
+++ app/src/main/java/com/stage3dev/bluenotify/NotifyService.java
@@ -0,0 +1,41 @@
+package com.stage3dev.bluenotify;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+/**
+ * Created by Kyle on 2/11/17.
+ */
+public class NotifyService extends NotificationListenerService {
+
+    private static final String TAG = NotifyService.class.getSimpleName();
+
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+        Log.i(TAG, "onListenerConnected()");
+
+    }
+
+    @Override
+    public void onListenerDisconnected() {
+        super.onListenerDisconnected();
+        Log.i(TAG, "onListenerDisconnected()");
+
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        Log.i(TAG,"**********  onNotificationPosted");
+        Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText + "t" + sbn.getPackageName());
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i(TAG,"********** onNOtificationRemoved");
+        Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText +"t" + sbn.getPackageName());
+
+    }
+}
